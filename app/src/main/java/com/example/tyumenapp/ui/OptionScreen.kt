1d8a0ff6cc09f36dfa6b0ba20310b411414133cc
Gogs <gogs@fake.local>
2025-03-19 14:16:41 +0500

    add dimes resource and update otpionScreen

--- app/src/main/java/com/example/tyumenapp/ui/OptionScreen.kt
+++ app/src/main/java/com/example/tyumenapp/ui/OptionScreen.kt
@@ -1,10 +1,67 @@
 package com.example.tyumenapp.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import com.example.tyumenapp.R
+import com.example.tyumenapp.model.Categories
+import com.example.tyumenapp.model.Recommendations
 
 @Composable
 fun OptionScreen() {
     Text(text = "option screen")
     //TODO: Заверстать экран
+}
+
+@Composable
+private fun CategoriesListItem(
+    categories: Categories,
+    recommendations: Recommendations,
+    onItemClick: (Recommendations) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        modifier = modifier,
+        onClick = { onItemClick(recommendations) }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(dimensionResource(R.dimen.card_image_height))
+        ) {
+
+        }
+    }
+}
+
+
+@Composable
+private fun CategoriesList(
+    categories: List<Categories>,
+    onClick: (Recommendations) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyColumn(
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+        modifier = modifier.padding(top = dimensionResource(R.dimen.padding_medium))
+    ) {
+        items(categories, key = { category -> category.id }) { category ->
+
+        }
+    }
 }
